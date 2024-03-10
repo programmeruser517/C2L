ff28ccbe4d2b4cd56fbbd9a422bccd845ccceb0e
Xiangbo Cai <caixian3@msu.edu>
2024-03-09 19:48:57 -0500

    main merge

--- connect2learn/lib/main.dart
+++ connect2learn/lib/main.dart
@@ -15,15 +15,16 @@ void main() {
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'C2L',
       theme: ThemeData(
-          // This is the theme of your application.
-          // ...
+        // This is the theme of your application.
+        // ...
           ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
       routes: {
@@ -41,7 +42,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -50,25 +51,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  
 //shamble
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+        return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+                        Text(
               'This is the Home Page',
               style: TextStyle(fontSize: 20.0),
             ),
@@ -82,6 +83,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-    );
+          );
   }
 }
