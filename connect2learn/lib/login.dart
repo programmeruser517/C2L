c69e4414b81e5241242672db5580b3d17d17854f
Krrish Seth <sethkrri@msu.edu>
2024-03-09 20:07:20 -0500

    login dart prelim code

--- connect2learn/lib/login.dart
+++ connect2learn/lib/login.dart
@@ -42,3 +42,26 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
+
+/**
+ * import 'package:flutter/material.dart';
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to the second screen using a named route.
+            Navigator.pushNamed(context, '/home');
+          },
+          child: const Text('Login'),
+        ),
+      ),
+    );
+  }
+}
+ */
